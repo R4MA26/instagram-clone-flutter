7425e86fba11bdf7742e8ff32018117c5989ff42
R4MA26 <ramaismaya25@gmail.com>
2022-02-17 13:48:29 +0800

    Add Post Screen UI

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/utils/global_variables.dart';
 // import 'package:instagram_flutter/providers/user_provider.dart';
 // import 'package:provider/provider.dart';
 // import 'package:instagram_flutter/models/user.dart' as model;
@@ -66,13 +67,7 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
 
     return Scaffold(
       body: PageView(
-        children: [
-          Text('feed'),
-          Text('search'),
-          Text('add post'),
-          Text('notif'),
-          Text('profile'),
-        ],
+        children: homeScreenItems,
         physics: const NeverScrollableScrollPhysics(),
         controller: pageController,
         onPageChanged: onPageChanged,
